0cd647f13a24fdcfbc91d7006ee1ad2178a84e63
Nilesh M <nilesh496@gmail.com>
2024-11-28 22:30:06 +0530

    ascii-art17

--- Ascii_art_generator/ascii_art.py
+++ Ascii_art_generator/ascii_art.py
@@ -11,7 +11,7 @@ def generate_ascii_art():
     font = input("Enter font style (or press Enter for default): ").strip()
 
     if font not in fonts:
-        print("Invalid or no font selected. Using default font.")
+        print("Invalid or no font was selected. Using default font.")
         font = "standard"
 
     # Generate ASCII art
