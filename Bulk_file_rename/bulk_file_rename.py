7c3e987edb8bf60d0d9f5c29fa3a12b3d8a4f777
Nilesh M <nilesh496@gmail.com>
2024-12-13 20:05:29 +0530

    bulk-file

--- Bulk_file_rename/bulk_file_rename.py
+++ Bulk_file_rename/bulk_file_rename.py
@@ -7,4 +7,4 @@ def bulk_rename(folder_path, old_name, new_name):
             os.rename(os.path.join(folder_path, file_name), os.path.join(folder_path, new_file_name))
             print(f"Renaming the files {file_name} to {new_file_name}")
 
-bulk_rename(r'D:\demo_files', 'Example', 'Samples')
+bulk_rename(r'D:\demo_files', 'Samples', 'SamplesNewSampler')
