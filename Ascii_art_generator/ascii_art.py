10189f801517047af6a63984af28d989344ef4cd
Nilesh M <nilesh496@gmail.com>
2025-01-22 21:21:49 +0530

    latest_ascii

--- Ascii_art_generator/ascii_art.py
+++ Ascii_art_generator/ascii_art.py
@@ -1,14 +1,12 @@
 from pyfiglet import Figlet
-
-
 def generate_ascii_art():
     # Choose a font style
     fonts = Figlet().getFonts()
     print("Available fonts:", ", ".join(fonts[:5]), "...and more!")
 
     # Input text and font
-    text = input("Enter the text to be converted to ASCII art: ")
-    font = input("Enter the font style (or press Enter for default): ").strip()
+    text = input("Enter the text to be converted to the ASCII art: ")
+    font = input("Enter the font style (or press the Enter key for default): ").strip()
 
     if font not in fonts:
         print("Invalid or no font was selected. Using the default font.")
