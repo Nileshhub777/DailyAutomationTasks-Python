9aa8ab5a6a16cd4075fcabcef6dd35f6b114a8b7
Nilesh M <nilesh496@gmail.com>
2025-02-15 23:13:01 +0530

    reversestring

--- reverse_string/reverse_string.py
+++ reverse_string/reverse_string.py
@@ -7,11 +7,9 @@ def reverse_string(string):
         reversed_str=char + reversed_str
     return reversed_str
 
-
 input_string="Hello"
 print("Original string:", input_string)
 print("reversed string:", reverse_string(input_string))
-
 
 
  # 2nd Way
