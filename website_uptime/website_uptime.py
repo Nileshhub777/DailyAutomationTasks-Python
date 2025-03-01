177066dd98523da10a65ba576e3d7fbce13ca821
Nilesh M <nilesh496@gmail.com>
2025-03-01 23:33:47 +0530

    website-up

--- website_uptime/website_uptime.py
+++ website_uptime/website_uptime.py
@@ -5,7 +5,7 @@ def check_website(url):
     try:
         response=requests.get(url)
         if response.status_code == 200:
-            print(f"Website {url} is up and running as usual")
+            print(f"Website {url} is up and running fine")
         else:
             print(f"Website {url} returned with the status : {response.status_code}")
     except requests.exceptions.RequestException as e:
