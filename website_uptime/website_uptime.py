5d7f0f2688e71b9db1b3242f5d7f76edbde46103
Nilesh M <nilesh496@gmail.com>
2025-03-03 20:47:04 +0530

    website-up

--- website_uptime/website_uptime.py
+++ website_uptime/website_uptime.py
@@ -7,9 +7,9 @@ def check_website(url):
         if response.status_code == 200:
             print(f"Website {url} is up and running fine")
         else:
-            print(f"Website {url} returned with the status : {response.status_code}")
+            print(f"Website {url} returned with status : {response.status_code}")
     except requests.exceptions.RequestException as e:
-        print(f"Error checking the website {url}: {e}")
+        print(f"Error checking this website {url}: {e}")
 
 
 url = 'https://www.geeksforgeeks.org/hangman-game-python/?ref=lbp'
