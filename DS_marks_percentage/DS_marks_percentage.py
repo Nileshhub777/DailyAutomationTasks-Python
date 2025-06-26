5ca646c7001a5cb5208a802697ea449f7acb4c66
Nilesh M <nilesh496@gmail.com>
2025-06-26 20:29:04 +0530

    Marks

--- DS_marks_percentage/DS_marks_percentage.py
+++ DS_marks_percentage/DS_marks_percentage.py
@@ -1,7 +1,7 @@
 students = [
     {"name": "Alice", "scores": [85, 92, 78, 88, 91,99]},  # Student 1
     {"name": "Bobby", "scores": [75, 80, 70, 85, 90,90]},    # Student 2
-    {"name": "Charmie", "scores": [99, 88, 84, 92, 85,88]}  # Student 3
+    {"name": "Charmyu", "scores": [99, 88, 84, 92, 85,88]}  # Student 3
 ]
 
 def calculate_scores_and_percentage(students):
