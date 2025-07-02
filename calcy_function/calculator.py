37337bbf28ce429f5607964bb224968a60a1c38e
Nilesh M <nilesh496@gmail.com>
2025-07-02 23:43:37 +0530

    Calculator

--- calcy_function/calculator.py
+++ calcy_function/calculator.py
@@ -34,7 +34,7 @@ while True:
             num1 = float(input("Enter the first number: "))
             num2 = float(input("Enter the second number: "))
         except ValueError:
-            print("Invalid input. Please enter a number.")
+            print("Invalid input... Please enter the number.")
             continue
 
         if choice == '1':
